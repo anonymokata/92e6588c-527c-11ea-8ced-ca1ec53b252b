98e99722be21f364657fe0e578dbb2be65b259d8
someone <someone>
2020-01-11 14:57:57 -0600

    Refactored paper.test.js. Updated PoA

--- test/paper.test.js
+++ test/paper.test.js
@@ -2,15 +2,14 @@ import {expect, assert} from 'chai';
 const PAPER = require('../src/paper');
 
 describe('tests for paper module', function(){
+    var sheet_one = PAPER.newPage();
     it('Paper.newPage() creates an empty instance of a piece of paper', function(){
-        expect(PAPER.newPage()).to.not.be.a('null');
+        expect(sheet_one).to.not.be.a('null');
     });
     it('Assigning Paper.newPage() assigns an object of type Paper', function(){
-        var sheet_one = PAPER.newPage();
         assert(sheet_one instanceof PAPER.Paper);
     });
     it("Calling Paper's method 'getText' returns a string", function() {
-        var sheet_one = PAPER.newPage();
         expect(sheet_one.getText()).to.be.a('string');
     });
 });
