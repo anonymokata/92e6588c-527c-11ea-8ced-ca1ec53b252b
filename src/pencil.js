5a381ed25f1c242e0407aa81285417c3df840e70
someone <someone>
2020-01-11 16:22:43 -0600

    Refactored pencil write method. Added helper class

--- src/pencil.js
+++ src/pencil.js
@@ -8,11 +8,17 @@ class Pencil {
     }
 
     write(text_to_write) {
-        var amount_to_reduce_length = text_to_write.trim().length;
-        var number_of_caps = text_to_write.replace(/[^A-Z]/g,"").length;
-        
-        this.point_durability = this.point_durability - (amount_to_reduce_length + number_of_caps);
+        //Would be own private function
+        const amount_to_reduce_length = calculatePointDurabilityReduction(text_to_write);
+        this.point_durability = (this.point_durability - amount_to_reduce_length);
     }
+}
+
+function calculatePointDurabilityReduction(text_to_write) {
+    const number_of_caps = text_to_write.replace(/[^A-Z]/g,"").length;
+    const no_space_text_length = text_to_write.trim().length;
+
+    return no_space_text_length + number_of_caps;
 }
 
 module.exports = {
